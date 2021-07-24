ba94840e282206e5308886f40bdebda6cc658315
Basti Hoffmann <bo.hoffi@gmail.com>
2021-07-24 14:41:13 +0200

    feat(proto-table): make paginator themable

--- libs/proto-table/src/lib/components/proto-table-paginator/proto-table-paginator.component.ts
+++ libs/proto-table/src/lib/components/proto-table-paginator/proto-table-paginator.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, ElementRef, HostBinding, Input } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'proto-ui-table-paginator',
@@ -9,4 +10,14 @@ export class ProtoTablePaginatorComponent {
 
   @HostBinding('class')
   public readonly hostClass = 'proto-ui-table-paginator';
+
+  @Input()
+  public set color(value: ThemePalette) {
+    const colorPalette = value || this.defaultColor;
+    this.elementRef.nativeElement.classList.add(`mat-${colorPalette}`);
+  }
+
+  private readonly defaultColor = undefined;
+
+  constructor(private readonly elementRef: ElementRef) { }
 }
